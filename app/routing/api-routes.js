3d33612f3518e90a291fe94e7ec50681ec503f76
Cooper Pearson <mptprogesterone@gmail.com>
2020-01-03 15:10:53 -0600

    first commit

---
+++ app/routing/api-routes.js
@@ -0,0 +1,46 @@
+var notesData = require ("../data/db.json");
+var fs = require ("fs");
+module.exports = function (app) {
+
+app.get("/api/notes", function (req, res) {
+    console.log("get data", notesData);
+
+    res.json(notesData);
+});
+
+//fs.writefile here to store to json file
+
+app.post("/api/notes", function (req, res) {
+    var id = notesData[notesData.length - 1].id +1;
+    var data = {
+        title: req.body.title, 
+        text: req.body.text,
+        id: id
+    };
+
+    notesData.push(data);
+    console.log("post data");
+    fs.writeFile("./app/data/db.json", JSON.stringify(notesData), function(err, data) {
+        
+        console.log("File updated")
+    });
+
+    res.json(true);
+});
+
+app.delete("/api/notes/:id", function (req, res) {
+var tempNotes = [];
+
+    for (let i =0; i <notesData.length; i++) {
+        if ( req.params.id !== notesData[i].id)
+        {tempNotes.push(notesData[i])}
+    };
+
+
+    notesData.delete(req.body);
+    res.json(true);
+});
+
+
+}
+
